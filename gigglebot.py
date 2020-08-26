81a3aa2782e1f2e16bd4e3d6ecd2fd5a71243470
Tom Gigler <tgigler@esri.com>
2020-08-25 22:46:42 -0700

    Try changing message

--- gigglebot.py
+++ gigglebot.py
@@ -7,6 +7,6 @@ async def on_message(message):
     if message.author == client.user:
         return
     if message.content == "Hello":
-        await message.channel.send('Hello yourself')
+        await message.channel.send(client.user)
 
 client.run("NzQ3ODM3NTM2Nzc2NDg2OTUz.X0Ur-g.sXBtS9DzLFq3oKWkfDsNs6tNCF0")
