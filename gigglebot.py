0092832118b9e3abf31b00ebfe78671809186a67
Tom Gigler <tgigler@esri.com>
2020-08-25 22:48:04 -0700

    Add time to output

--- gigglebot.py
+++ gigglebot.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 import discord
+import time
 import re
 
 client = discord.Client()
@@ -9,6 +10,6 @@ async def on_message(message):
     if message.author == client.user:
         return
     if re.search('hello', message.content, re.IGNORECASE):
-        await message.channel.send(client.user)
+        await message.channel.send(f"{message.id} {time.strftime('%X')}")
 
 client.run("NzQ3ODM3NTM2Nzc2NDg2OTUz.X0Ur-g.rKAZTJh8jmqbKzYKy95zfWKJh9M")
