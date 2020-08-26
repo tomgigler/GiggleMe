4b1fecde842580fff64deff9b9859c79eabff3e7
Tom Gigler <tgigler@esri.com>
2020-08-25 22:43:59 -0700

    Add __main__

--- gigglebot.py
+++ gigglebot.py
@@ -8,4 +8,5 @@ async def on_message(message):
     if message.content == "Hello":
         await message.channel.send('Hello yourself')
 
-client.run("NzQ3ODM3NTM2Nzc2NDg2OTUz.X0Ur-g.sXBtS9DzLFq3oKWkfDsNs6tNCF0")
+if __name__ == "__main__":
+    client.run("NzQ3ODM3NTM2Nzc2NDg2OTUz.X0Ur-g.sXBtS9DzLFq3oKWkfDsNs6tNCF0")
