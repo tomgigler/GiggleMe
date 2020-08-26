7204694b8e85e3cf68d0c60959d448c638da0ba3
Tom Gigler <tgigler@esri.com>
2020-08-25 22:46:20 -0700

    Remove __main__

--- gigglebot.py
+++ gigglebot.py
@@ -9,5 +9,4 @@ async def on_message(message):
     if message.content == "Hello":
         await message.channel.send('Hello yourself')
 
-if __name__ == "__main__":
-    client.run("NzQ3ODM3NTM2Nzc2NDg2OTUz.X0Ur-g.sXBtS9DzLFq3oKWkfDsNs6tNCF0")
+client.run("NzQ3ODM3NTM2Nzc2NDg2OTUz.X0Ur-g.sXBtS9DzLFq3oKWkfDsNs6tNCF0")
