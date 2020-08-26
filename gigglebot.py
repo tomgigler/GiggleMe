f751ea819e72e9e845c6f06f7189b3594c973707
Tom Gigler <tgigler@esri.com>
2020-08-26 09:07:12 -0700

    Add temp conversion

--- gigglebot.py
+++ gigglebot.py
@@ -4,6 +4,7 @@ import time
 import re
 import os
 import asyncio
+import sys
 
 client = discord.Client()
 
@@ -11,6 +12,13 @@ client = discord.Client()
 async def on_message(message):
     if message.author == client.user:
         return
+    if re.search('^[-]?[0-9][0-9]?[0-9]?[CF]$', message.content):
+        match = re.search('([-]?[0-9][0-9]?[0-9]?)([CF])', message.content)
+        temp = float(match.group(1))
+        scale = match.group(2)
+        newtemp = temp * 1.8 + 32 if scale == 'C' else (temp - 32)/1.8
+        newscale = 'F' if scale == 'C' else 'C'
+        await message.channel.send(f"{temp}{scale} = {round(newtemp, 1)}{newscale}")
     if re.search('hello', message.content, re.IGNORECASE):
         await asyncio.sleep(6)
         await message.channel.send(f"{message.content} {time.strftime('%X')}")
