b7e0e6bba7e0164456f6217de703927ceca90d80
Tom Gigler <tgigler@esri.com>
2020-08-25 22:47:09 -0700

    Search case insensitive and allow leading and trailing text

--- gigglebot.py
+++ gigglebot.py
@@ -1,12 +1,14 @@
 #!/usr/bin/env python
 import discord
+import re
+
 client = discord.Client()
 
 @client.event
 async def on_message(message):
     if message.author == client.user:
         return
-    if message.content == "Hello":
+    if re.search('hello', message.content, re.IGNORECASE):
         await message.channel.send(client.user)
 
 client.run("NzQ3ODM3NTM2Nzc2NDg2OTUz.X0Ur-g.sXBtS9DzLFq3oKWkfDsNs6tNCF0")
