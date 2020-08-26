810a5250d8080690e2d13bd9868367fb87b27489
Tom Gigler <tgigler@esri.com>
2020-08-26 13:36:26 -0700

    Remove all functionality except temp conversion

--- gigglebot.py
+++ gigglebot.py
@@ -12,8 +12,6 @@ client = discord.Client()
 async def on_message(message):
     if message.author == client.user:
         return
-    if message.content == 'kill':
-        sys.exit()
     if re.search('[-]?[0-9]+[CF]', message.content):
         for temp in re.findall('[-]?[0-9]+[CF]', message.content):
             match = re.search('([-]?[0-9]+)([CF])', temp)
@@ -22,16 +20,5 @@ async def on_message(message):
             newvalue = value * 1.8 + 32 if scale == 'C' else (value - 32)/1.8
             newscale = 'F' if scale == 'C' else 'C'
             await message.channel.send(f"{value}{scale} = {round(newvalue, 1)}{newscale}")
-    if re.search('hello', message.content, re.IGNORECASE):
-        await asyncio.sleep(6)
-        await message.channel.send(f"{message.content} {time.strftime('%X')}")
-    elif re.search('bye', message.content, re.IGNORECASE):
-        await message.channel.send(f"{message.content} {time.strftime('%X')}")
-    elif message.content == 'channels':
-        for channel in message.guild.channels:
-            await message.channel.send(f"{str(channel)}: {channel.type}")
-    elif message.content == 'delete':
-        async for message in message.channel.history(limit=200):
-            await message.delete()
 
 client.run(os.getenv('BOT_TOKEN'))
