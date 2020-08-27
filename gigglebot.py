d57c48115a19dd570f54d684a82fa72bd6316cfc
Tom Gigler <tgigler@esri.com>
2020-08-27 08:20:59 -0700

    Move temp processing to separate method

--- gigglebot.py
+++ gigglebot.py
@@ -6,25 +6,29 @@ import asyncio
 
 client = discord.Client()
 
+async def process_temps(message):
+    processed_values = []
+    for temp in re.findall('[-]?[0-9]*\.?[0-9]+ ?[CFcf](?:\s+|$)', message.content):
+        match = re.search('([-]?[0-9]*\.?[0-9]+) ?([CFcf])(?:\s+|$)', temp)
+        value = float(match.group(1))
+        scale = match.group(2)
+        if scale == 'c':
+            scale = 'C'
+        if scale == 'f':
+            scale = 'F'
+        newvalue = value * 1.8 + 32 if scale == 'C' else (value - 32)/1.8
+        newscale = 'F' if scale == 'C' else 'C'
+        if f"{value}{scale}" not in processed_values:
+            await message.channel.send(f"{value} {scale} = {round(newvalue, 1)} {newscale}")
+            processed_values.append(f"{value}{scale}")
+            processed_values.append(f"{newvalue}{newscale}")
+
+
 @client.event
 async def on_message(message):
     if message.author == client.user:
         return
     if re.search('[-]?[0-9]*\.?[0-9]+ ?[CFcf](?:\s+|$)', message.content):
-        processed_values = []
-        for temp in re.findall('[-]?[0-9]*\.?[0-9]+ ?[CFcf](?:\s+|$)', message.content):
-            match = re.search('([-]?[0-9]*\.?[0-9]+) ?([CFcf])(?:\s+|$)', temp)
-            value = float(match.group(1))
-            scale = match.group(2)
-            if scale == 'c':
-                scale = 'C'
-            if scale == 'f':
-                scale = 'F'
-            newvalue = value * 1.8 + 32 if scale == 'C' else (value - 32)/1.8
-            newscale = 'F' if scale == 'C' else 'C'
-            if f"{value}{scale}" not in processed_values:
-                await message.channel.send(f"{value} {scale} = {round(newvalue, 1)} {newscale}")
-                processed_values.append(f"{value}{scale}")
-                processed_values.append(f"{newvalue}{newscale}")
+        await process_temps(message)
 
 client.run(os.getenv('BOT_TOKEN'))
