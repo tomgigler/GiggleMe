0fa7a14389d6689a737da64bb77762b01a8c7870
Tom Gigler <tgigler@esri.com>
2021-01-03 01:20:06 -0800

    Remove old script

--- restart-gigglebot.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-export PYTHONPATH=${PYTHONPATH}:$(pwd):$(pwd)/util
-
-ps -ef | grep $(pwd)/gigglebot.py | grep -v grep | awk  '{print "kill " $2}' | /bin/bash
-
-python3 $(pwd)/gigglebot.py &
-
