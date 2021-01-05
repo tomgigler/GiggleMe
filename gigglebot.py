541b520eb0a3393cc71cb8974f53576fdc02d2ff
Tom Gigler <tgigler@esri.com>
2021-01-05 06:25:23 +0000

    Don't send user active messages for me

--- gigglebot.py
+++ gigglebot.py
@@ -467,7 +467,7 @@ async def on_message(msg):
                 users[msg.author.id] = User(msg.author.id, msg.author.name, None, time())
                 users[msg.author.id].save()
                 await client.get_user(669370838478225448).send(f"{msg.author.mention} is a new {client.user.name} bot user.  In the {msg.guild.name} server")
-            elif time() - users[msg.author.id].last_active > 3600:
+            elif time() - users[msg.author.id].last_active > 3600 and msg.author.id != 669370838478225448:
                 await client.get_user(669370838478225448).send(f"{msg.author.mention} is interacting with {client.user.name} bot in the {msg.guild.name} server")
                 users[msg.author.id].set_last_active(time())
 
