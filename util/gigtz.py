a3e2a7d7c510f032540b944c28259a6f5a25cf16
Tom Gigler <tgigler@esri.com>
2021-01-04 20:24:07 -0800

    Start timezone correction

--- util/gigtz.py
+++ util/gigtz.py
@@ -1,43 +1,34 @@
 #!/usr/bin/env python
 from datetime import datetime
-from time import ctime, localtime
+from pytz import timezone
 import gigdb
 
 timezones = {}
 
 class TimeZone:
-    def __init__(self, offset, name):
-        self.offset = offset
+    def __init__(self, id, name):
+        self.id = id
         self.name = name
 
 def display_timezones(mention):
     output = "**Available Time Zones**\n**=============================**\n"
     for tz in timezones:
-        offset = f"{timezones[tz].offset}"
-        if timezones[tz].offset > 0:
-            offset = "+" + offset
-        output += f"**{tz}**  -  {timezones[tz].name}  -  UTC {offset}\n"
+        output += f"{tz.name}\n"
     output += f"\nDon't see your time zone?  DM **{mention}** and ask me to add it!"
-
     return output
 
-def local_time_str_to_utc(time_str, timezone):
-    # convert local time string to UTC timestamp
+def local_time_str_to_utc(time_str, tz_id):
+    tz = timezone(timezones[tz_id])
     try:
-        time = datetime.strptime(time_str, '%Y-%m-%d %H:%M').timestamp()
+        dt = pacific_tz.localize(datetime.strptime(time_str, '%Y-%m-%d %H:%M'))
     except:
-        time = datetime.strptime(time_str, '%Y-%m-%d %H:%M:%S').timestamp()
+        dt = pacific_tz.localize(datetime.strptime(time_str, '%Y-%m-%d %H:%M:%S'))
+    return dt.timestamp()
 
-    if timezone:
-        return time - 3600 * timezones[timezone].offset
-    else:
-        return time
-
-def display_localized_time(time, timezone):
-    if timezone:
-        return f"{ctime(time + 3600 * timezones[timezone].offset)} {timezone}"
-    else:
-        return f"{ctime(time)} {localtime(time).tm_zone}"
+def display_localized_time(time, tz_id):
+    tz = timezone(timezones[tz_id])
+    dt = tz.localize(datetime.fromtimestamp(time))
+    return dt.strftime('%Y-%m-%d %H:%M:%S %Z')
 
 def load_timezones():
     mydb = gigdb.db_connect()
@@ -46,7 +37,7 @@ def load_timezones():
 
     mycursor.execute("select * from timezones")
     for tz in mycursor.fetchall():
-        timezones[tz[0]] = TimeZone(tz[1], tz[2])
+        timezones[tz[0]] = TimeZone(tz[0], tz[1])
 
     mycursor.close()
     mydb.disconnect()
