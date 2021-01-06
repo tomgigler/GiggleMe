f1311ee56a62fd469934543581b58ce3aa09b930
Tom Gigler <tgigler@esri.com>
2021-01-06 14:17:17 +0000

    Fix formatting

--- util/gigdb.py
+++ util/gigdb.py
@@ -4,7 +4,8 @@ import mysql.connector
 
 def db_connect():
     return mysql.connector.connect(
-            host="localhost",            user=settings.db_user,
+            host="localhost",
+            user=settings.db_user,
             password=settings.db_password,
             database=settings.database,
             charset='utf8mb4'
