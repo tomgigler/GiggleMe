1cc87d23c770ffe0746f8f262b5e8aeac9478a86
Tom Gigler <tgigler@esri.com>
2021-01-10 15:50:32 +0000

    Handle param wrapped in double qoutes

--- util/gigparse.py
+++ util/gigparse.py
@@ -3,7 +3,10 @@ import re
 
 async def parse_args(func, params, param_str):
     while param_str:
-        match = re.match(r'((\S+)=(\S+) *)', param_str)
+        # first look for arg surrounded by quotes
+        match = re.match(r'((\S+)="([^"]+)" *)', param_str)
+        if not match:
+            match = re.match(r'((\S+)=(\S+) *)', param_str)
         params[match.group(2)] = match.group(3)
         param_str = param_str.replace(match.group(1), "")
 
