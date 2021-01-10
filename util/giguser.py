80a9c58f273f4b512c7dcc46dbe71f200beec6b6
Tom Gigler <tgigler@esri.com>
2021-01-09 18:21:12 -0800

    Add save_user

--- util/giguser.py
+++ util/giguser.py
@@ -67,3 +67,49 @@ def load_users():
     mycursor.close()
     mydb.disconnect()
 
+def save_user(user_id, name, guild_id, guild_name):
+
+    mydb = db_connect()
+    mycursor = mydb.cursor(buffered=True)
+
+    sql = "SELECT * FROM users WHERE id = %s"
+
+    mycursor.execute(sql, ( user_id, ))
+
+    if mycursor.rowcount > 0:
+        update = True
+    else:
+        update = False
+
+    if update:
+        sql = "UPDATE users SET name = %s WHERE user = %s"
+    else:
+        sql = "INSERT INTO users ( name, user ) values ( %s, %s )"
+
+    mycursor.execute(sql, ( name, user_id ) )
+
+    sql = "SELECT * FROM user_guilds WHERE user_id = %s and guild_id = %s"
+
+    mycursor.execute(sql, ( user_id, guild_id ) )
+
+    if mycursor.rowcount > 0:
+        update = True
+    else:
+        update = False
+
+    if update:
+        sql = "UPDATE user_guilds SET guild_name = %s WHERE user_id = %s and guild_id = %s"
+    else:
+        sql = "INSERT INTO user_guilds ( guild_name, user_id, guild_id ) values ( %s, %s, %s )"
+
+    mycursor.execute(sql, ( guild_name, user_id, guild_id ) )
+
+    mydb.commit()
+
+    mycursor.close()
+    mydb.disconnect()
+
+    if user_id in user_guilds.keys():
+        user_guilds[user_id].append(guild_id)
+    else:
+        user_guilds[user_id] = [ guild_id ]
