3c424090c55641c7b95745fecd43b66f3183e689
Tom Gigler <tgigler@esri.com>
2021-01-15 16:00:25 +0000

    Move sql from gigvote.py to gigdb.py

--- util/gigdb.py
+++ util/gigdb.py
@@ -11,19 +11,34 @@ def db_connect():
             charset='utf8mb4'
             )
 
-def db_get_rows(sql, **kwargs):
+def db_execute_sql(sql, fetch, **kwargs):
     mydb = db_connect()
 
-    mycursor = mydb.cursor()
+    mycursor = mydb.cursor(buffered=True)
 
     mycursor.execute(sql, tuple(kwargs.values()))
 
-    rows =  mycursor.fetchall()
+    rows = None
+    if fetch:
+        rows = mycursor.fetchall()
 
+    mydb.commit()
     mycursor.close()
     mydb.disconnect()
 
     return rows
 
 def get_messages():
-    return db_get_rows("select * from messages")
+    return db_execute_sql("SELECT * FROM messages", True)
+
+def get_votes(proposal_id):
+    return db_execute_sql("SELECT * FROM votes WHERE proposal_id = %s", True, proposal_id=proposal_id)
+
+def delete_proposal_votes(proposal_id):
+    db_execute_sql("DELETE FROM votes WHERE proposal_id = %s", False, proposal_id=proposal_id)
+
+def replace_vote(proposal_id, user_id, vote):
+    db_execute_sql("INSERT INTO votes (proposal_id, user_id, vote) VALUES(%s, %s, %s) ON DUPLICATE KEY UPDATE vote=%s", False, proposal_id=proposal_id, user_id=user_id, vote_1=vote, vote_2=vote)
+            
+def remove_vote(proposal_id, user_id):
+    db_execute_sql("DELETE FROM votes WHERE proposal_id = %s and user_id = %s", False, proposal_id=proposal_id, user_id=user_id)
