e8f081328123cfee0e3e9b2db29b2acb4be8489e
Tom Gigler <tgigler@esri.com>
2021-01-27 16:22:48 +0000

    Add Channel column

--- web/home.php
+++ web/home.php
@@ -11,15 +11,22 @@ print "<br><br>\n";
 
 date_default_timezone_set($_SESSION['timezone']);
 $connection = new mysqli("localhost", $db_user, $db_pass, $db_name);
-$messages = $connection->query("SELECT m.id, g.guild_name, u.name, m.delivery_time, m.repeats, m.repeat_until, m.description FROM messages AS m, guilds AS g, users AS u WHERE m.guild_id = g.id AND m.author_id = u.user AND m.delivery_time >= 0 ORDER BY delivery_time");
+$sql = <<<SQL
+SELECT m.id, g.guild_name, c.name, u.name, m.delivery_time, m.repeats, m.repeat_until, m.description
+FROM messages AS m, guilds AS g, users AS u, channels AS c
+WHERE m.guild_id = g.id AND m.author_id = u.user AND m.delivery_time >= 0 AND c.id = m.delivery_channel_id
+ORDER BY delivery_time
+SQL;
+$messages = $connection->query($sql);
 
 print "<center>\n";
 if(mysqli_num_rows($messages)){
 print "<table border=1>\n";
-print "  <tr><th colspan=7><b>Messages</b></th></tr>\n";
+print "  <tr><th colspan=8><b>Messages</b></th></tr>\n";
 print "  <tr>\n";
 print "    <th>Message ID</th>\n";
 print "    <th>Server Name</th>\n";
+print "    <th>Channel</th>\n";
 print "    <th>Author</th>\n";
 print "    <th>Delivery Time</th>\n";
 print "    <th>Repeats</th>\n";
@@ -29,16 +36,29 @@ print "  </tr>\n";
 while($row = $messages->fetch_row()) {
   print "  <tr>\n";
   // 7:00:00 PM Mon Jan 25, 2021 PST
-  $delivery_time = date("g:i:s A D M j, Y T",$row[3]);
-  if($row[5]) { $repeat_until = date("g:i:s A D M j, Y T",$row[5]); } else { $repeat_until = $row[5]; }
-  print "<td class='message-id' onclick=\"location.href='message.php?id=$row[0]'\">$row[0]</td><td>$row[1]</td><td>$row[2]</td><td>$delivery_time</td><td>$row[4]</td><td>$repeat_until</td><td>$row[6]</td>\n";
+  $delivery_time = date("g:i:s A D M j, Y T",$row[4]);
+  if($row[6]) { $repeat_until = date("g:i:s A D M j, Y T",$row[6]); } else { $repeat_until = $row[6]; }
+  print "    <td class='link-cell' onclick=\"location.href='message.php?id=$row[0]'\">$row[0]</td>\n";
+  print "    <td>$row[1]</td>\n";
+  print "    <td>$row[2]</td>\n";
+  print "    <td>$row[3]</td>\n";
+  print "    <td>$delivery_time</td>\n";
+  print "    <td>$row[5]</td>\n";
+  print "    <td>$repeat_until</td>\n";
+  print "    <td>$row[7]</td>\n";
   print "  </tr>\n";
 }
 print "</table>\n";
 print "<br><br>\n";
 }
 
-$templates = $connection->query("SELECT m.id, g.guild_name, u.name, m.description FROM messages AS m, guilds AS g, users AS u WHERE m.guild_id = g.id AND m.author_id = u.user AND m.delivery_time is NULL ORDER BY delivery_time");
+$sql = <<<SQL
+SELECT m.id, g.guild_name, c.name, u.name, m.description, g.id, c.id
+FROM messages AS m, guilds AS g, users AS u, channels AS c
+WHERE m.guild_id = g.id AND m.author_id = u.user AND m.delivery_time is NULL AND c.id = m.delivery_channel_id
+ORDER BY delivery_time
+SQL;
+$templates = $connection->query($sql);
 
 if(mysqli_num_rows($templates)){
 print "<table border=1>\n";
@@ -46,13 +66,19 @@ print "  <tr><th colspan=5><b>Templates</b></th></tr>\n";
 print "  <tr>\n";
 print "    <th>Template ID</th>\n";
 print "    <th>Server Name</th>\n";
+print "    <th>Channel</th>\n";
 print "    <th>Author</th>\n";
 print "    <th>Description</th>\n";
 print "  </tr>\n";
 while($row = $templates->fetch_row()) {
   print "  <tr>\n";
   # 12:30:00 PM Sun Jan 24, 2021 MST
-  print "<td class='message-id' onclick=\"location.href='template.php?id=$row[0]'\">$row[0]</td><td>$row[1]</td><td>$row[2]</td><td>$row[3]</td>\n";
+  # https://discord.com/channels/{guild_id}/{channel_id}
+  print "    <td class='link-cell' onclick=\"location.href='template.php?id=$row[0]'\">$row[0]</td>\n";
+  print "    <td>$row[1]</td>\n";
+  print "    <td>$row[2]</td>\n";
+  print "    <td>$row[3]</td>\n";
+  print "    <td>$row[4]</td>\n";
   print "  </tr>\n";
 }
 print "</table>\n";
