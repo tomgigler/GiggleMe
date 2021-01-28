e0131da8c9ea0ed3fadcb11d76182859bc977b5d
Tom Gigler <tgigler@esri.com>
2021-01-28 13:56:25 +0000

    Only show messages for guilds user has access to

--- web/home.php
+++ web/home.php
@@ -10,11 +10,13 @@ print "</center>\n";
 print "<br><br>\n";
 
 date_default_timezone_set($_SESSION['timezone']);
+$user_id = intval($_SESSION['user_id']);
 $connection = new mysqli("localhost", $db_user, $db_pass, $db_name);
 $sql = <<<SQL
 SELECT m.id, g.guild_name, c.name, u.name, m.delivery_time, m.repeats, m.repeat_until, m.description
 FROM messages AS m, guilds AS g, users AS u, channels AS c
 WHERE m.guild_id = g.id AND m.author_id = u.user AND m.delivery_time >= 0 AND c.id = m.delivery_channel_id
+AND g.id in ( SELECT guild_id FROM user_guilds WHERE user_id = $user_id )
 ORDER BY delivery_time
 SQL;
 $messages = $connection->query($sql);
@@ -56,6 +58,7 @@ $sql = <<<SQL
 SELECT m.id, g.guild_name, c.name, u.name, m.description, g.id, c.id
 FROM messages AS m, guilds AS g, users AS u, channels AS c
 WHERE m.guild_id = g.id AND m.author_id = u.user AND m.delivery_time is NULL AND c.id = m.delivery_channel_id
+AND g.id in ( SELECT guild_id FROM user_guilds WHERE user_id = $user_id )
 ORDER BY delivery_time
 SQL;
 $templates = $connection->query($sql);
