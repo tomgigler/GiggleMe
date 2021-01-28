5df4ee615611ad7d2e855ba57f77861096811f13
Tom Gigler <tgigler@esri.com>
2021-01-28 13:55:53 +0000

    Save user_id in SESSION

--- web/Login.php
+++ web/Login.php
@@ -15,13 +15,14 @@ class Login {
       $this->db = new DBConnection();
       $this->db->connect() or die('There was an error connecting to the database.');
 
-      $stmt = $this->db->connection->prepare("SELECT users.name, timezones.name FROM users, timezones WHERE users.name = ? AND users.timezone = timezones.id AND users.password = PASSWORD(?)");
+      $stmt = $this->db->connection->prepare("SELECT users.name, timezones.name, users.user FROM users, timezones WHERE users.name = ? AND users.timezone = timezones.id AND users.password = PASSWORD(?)");
       $stmt->bind_param('ss', $user, $pass);
       $stmt->execute();
       $result = $stmt->get_result();
       $ret = mysqli_num_rows($result);
       $row = $result->fetch_row();
       $_SESSION['timezone'] = $row[1];
+      $_SESSION['user_id'] = $row[2];
 
       $this->db->close();
 
