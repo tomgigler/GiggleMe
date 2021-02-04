68695754e67ab2c91dbe268e95874bf8115b2492
Tom Gigler <tgigler@esri.com>
2021-02-03 18:43:30 -0800

    Persist login (#128)
    * Add remember me option
    
    * Make persistent login more intuitive

--- web/logout.php
+++ web/logout.php
@@ -1,19 +1,11 @@
 <?php
+
    session_start();
-   if (isset($_SESSION['USER']) || isset($_SESSION['PASS']))
-   {
-      $message = "    <font color='green'><b>You have been successfully logged out.</b></font>\n";
-   }
-   $_SESSION = array();
-   if (isset($_COOKIE[session_name()]))
-     setcookie(session_name(), '', time()-42000, '/');
+   if (isset($_COOKIE[$_SESSION['DATABASE']]))
+     setcookie($_SESSION['DATABASE']);
    session_destroy();
 
-  $host = $_SERVER['HTTP_HOST'];
-  $uri = rtrim(dirname($_SERVER['PHP_SELF']), '/\\');
   header("Location: index.php");
   exit;
 
 ?>
-
-
