f232b2fa65eb79f9932f9b8bfe26a20dd0ffd45a
Tom Gigler <tgigler@esri.com>
2021-02-10 23:06:40 +0000

    Use Message class in message_page.php

--- web/message_page.php
+++ web/message_page.php
@@ -2,13 +2,14 @@
 include "login_check.php";
 include "header.php";
 require_once "DBConnection.php";
+require_once "Message.php";
 
 date_default_timezone_set($_SESSION['timezone']);
 if(isset($_GET['id'])) $msg_id = $_GET['id'];
 else $msg_id = substr(md5(time()),0,8);
 
 $db = new DBConnection();
-$message = $db->get_message($msg_id);
+$message = Message::get_message_by_id($msg_id);
 
 $channels = array();
 $templates = array();
@@ -58,13 +59,13 @@ print "    <th>Author</th>\n";
 if($_GET['action']=='create'){
   print "    <td>".$_SESSION['username']."</td>\n";
 } else {
-  print "    <td>".htmlspecialchars($message[1])."</td>\n";
+  print "    <td>".htmlspecialchars($message->author)."</td>\n";
 }
 print "  </tr>\n";
 
 print "  <tr id='server_row'>\n";
 print "    <th>Server</th>\n";
-print "    <td id='display_server_cell' class='display_element'>".htmlspecialchars($message[2])."</td>\n";
+print "    <td id='display_server_cell' class='display_element'>".htmlspecialchars($message->server)."</td>\n";
 print "    <td id='edit_server_cell' class='edit_element'>\n";
 print "      <select id='server_select' style='display:table-cell; width:100%' onchange='server_select_updated()'>\n";
 foreach($servers as $server){
@@ -76,7 +77,7 @@ print "  </tr>\n";
 
 print "  <tr id='channel_row'>\n";
 print "    <th>Channel</th>\n";
-print "    <td id='display_channel_cell' class='display_element'>".htmlspecialchars($message[3])."</td>\n";
+print "    <td id='display_channel_cell' class='display_element'>".htmlspecialchars($message->channel)."</td>\n";
 print "    <td id='edit_channel_cell' class='edit_element'>\n";
 print "      <select id='channel_select' style='display:table-cell; width:100%' />\n";
 print "    </td>\n";
@@ -84,7 +85,7 @@ print "  </tr>\n";
 
 print "  <tr id='delivery_time_row'>\n";
 print "    <th>Delivery Time</th>\n";
-print "    <td id='display_delivery_time_cell' class='display_element'>".date("g:i:s A D M j, Y T",$message[4])."</td>\n";
+print "    <td id='display_delivery_time_cell' class='display_element'>".$message->delivery_time_format."</td>\n";
 print "    <td id='edit_delivery_time_cell' class='edit_element'><input id='delivery_time' type=datetime-local /></td>\n";
 print "  </tr>\n";
 
@@ -97,7 +98,7 @@ print "  </tr>\n";
 
 print "  <tr id='repeats_row'>\n";
 print "    <th>Repeats</th>\n";
-print "    <td id='display_repeats_cell' class='display_element'>".$message[5]."</td>\n";
+print "    <td id='display_repeats_cell' class='display_element'>".$message->repeats."</td>\n";
 print "    <td id='edit_repeats_cell' class='edit_element'>\n";
 print "      <select id='repeats_select' style='display:table-cell; width:50%' onchange='update_repeats_select()'>\n";
 print "        <option selected value='None'>None</option>\n";
@@ -122,7 +123,7 @@ print "  </tr>\n";
 print "  <tr id='repeat_until_row'>\n";
 print "    <th>Repeat Until</th>\n";
 print "    <td id='display_repeat_until_cell' class='display_element'>";
-if($message[6]) print date("g:i:s A D M j, Y T",$message[6]);
+if($message->repeat_until) print $message->repeat_until_format;
 print "</td>\n";
 print "    <td id='edit_repeat_until_cell' class='edit_element'>\n";
 print "      <input id='repeat_until_checkbox' type='checkbox' onchange='toggle_repeat_until_datetime()' />\n";
@@ -132,7 +133,7 @@ print "  </tr>\n";
 
 print "  <tr id='description_row'>\n";
 print "    <th>Description</th>\n";
-print "    <td id='display_description_cell' class='display_element'>".htmlspecialchars($message[7])."</td>\n";
+print "    <td id='display_description_cell' class='display_element'>".htmlspecialchars($message->description)."</td>\n";
 print "    <td  id='edit_description_cell' class='edit_element'><input id='description' style='display:table-cell; width:100%' /></td>\n";
 print "  </tr>\n";
 
@@ -143,7 +144,7 @@ print "<textarea id='edit_content' class='edit_element' cols='124' rows='24' max
 print "</textarea>\n";
 print "</center>";
 print "<div id='display_content_div' class='display_element content-div'>\n";
-print "  <pre id='display_content_pre'>".htmlspecialchars($message[8])."</pre>\n";
+print "  <pre id='display_content_pre'>".htmlspecialchars($message->content)."</pre>\n";
 print "</div>\n";
 
 print "<script>\n";
@@ -151,39 +152,17 @@ $js_channels = json_encode($channels);
 echo "var channels = ". $js_channels . ";\n";
 $js_templates = json_encode($templates);
 echo "var templates = ". $js_templates . ";\n";
-print "var guild_id = '" . $message[9] . "'\n";
-print "var channel_id = '" . $message[10] . "'\n";
-if($message[4]){
-  print "var delivery_time_java_format = '" . date("Y-m-d\TH:i",$message[4]) . "'\n";
-} else {
-  print "var delivery_time_java_format = ''\n";
-}
-if($message[6]){
-  print "var repeat_until_java_format = '" . date("Y-m-d\TH:i",$message[6]) . "'\n";
-} else {
-  print "var repeat_until_java_format = ''\n";
-}
+print "var guild_id = '" . $message->guild_id . "'\n";
+print "var channel_id = '" . $message->delivery_channel_id . "'\n";
+print "var delivery_time_java_format = '" . $message->delivery_time_java_format . "'\n";
+print "var repeat_until_java_format = '" . $message->repeat_until_java_format . "'\n";
 
-if($message[5]){
-  $repeat_frequency_full = preg_replace("/;.*/", "", $message[5]);
-  $repeat_frequency = preg_replace("/:.*/", "", $repeat_frequency_full);
-  if(preg_match("/:/", $repeat_frequency_full)){
-    $repeat_frequency_num = preg_replace("/.*:/", "", $repeat_frequency_full);
-  } else {
-    $repeat_frequency_num = "";
-  }
-  if(preg_match("/=/", $message[5])){
-    $repeat_skip_if = preg_replace("/.*=/", "", $message[5]);
-  } else {
-    $repeat_skip_if = "";
-  }
-}
-print "var repeat_frequency = '" . $repeat_frequency . "'\n";
-print "var repeat_frequency_num = '" . $repeat_frequency_num . "'\n";
-print "var repeat_skip_if = '" . $repeat_skip_if . "'\n";
+print "var repeat_frequency = '" . $message->repeat_frequency . "'\n";
+print "var repeat_frequency_num = '" . $message->repeat_frequency_num . "'\n";
+print "var repeat_skip_if = '" . $message->repeat_skip_if . "'\n";
 
 if(!$_GET['action']=='create'){
-  if(!$message[4]){
+  if(!$message->delivery_time){
     print "$('#message_id_header_cell').text('Template ID')\n";
     print "$('#delivery_time_row').toggle(false)\n";
     print "$('#message_type_select').val('template')\n";
@@ -203,7 +182,7 @@ if($_GET['action']=='create') print "var creating_new_message=true\n";
 else print "var creating_new_message=false\n";
 
 print "$(function(){\n";
-print "  load_message_page('".$_GET['action']."', '".$message[6]."');\n";
+print "  load_message_page('".$_GET['action']."', '".$message->repeat_until."');\n";
 print "});\n";
 ?>
 setInputFilter(document.getElementById("repeats_num"), function(value) {
