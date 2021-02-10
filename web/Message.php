a3bd37bc0373ad00cc7c46cfda42a77ff0ad74c2
Tom Gigler <tgigler@esri.com>
2021-02-10 14:39:26 +0000

    Create batch message from Message class

--- web/Message.php
+++ web/Message.php
@@ -2,6 +2,8 @@
 
 require_once "DBConnection.php";
 
+class BadRequestException extends Exception {}
+
 class Message {
 
   function Message($id, $guild_id, $delivery_channel_id, $delivery_time, $author_id, $repeats, $content, $description, $repeat_until){
@@ -14,14 +16,6 @@ class Message {
     $this->content = $content;
     $this->description = $description;
     $this->repeat_until = $repeat_until;
-  }
-
-  static function get_message_by_id($id){
-    $db = new DBConnection();
-    $msg = $db->get_message_by_id($id);
-    $message = new Message($msg[0], $msg[1], $msg[2], $msg[3], $msg[4], $msg[5], $msg[7], $msg[8], $msg[9]);
-    $message->set_all();
-    return $message;
   }
 
   function guild_name(){
@@ -96,6 +90,32 @@ class Message {
     $this->repeat_frequency_num = $this->repeat_frequency_num();
     $this->repeat_skip_if = $this->repeat_skip_if();
   }
-}
 
+  static function get_message_by_id($id){
+    $db = new DBConnection();
+    $msg = $db->get_message_by_id($id);
+    $message = new Message($msg[0], $msg[1], $msg[2], $msg[3], $msg[4], $msg[5], $msg[7], $msg[8], $msg[9]);
+    $message->set_all();
+    return $message;
+  }
+
+  static function create_message_from_command($cmd, $guild_id){
+    $db = new DBConnection();
+    [ $command, $content ] = explode("\n", $cmd, 2);
+    // ~giggle 2021-02-05 18:00 channel=truth-wanted desc=\"TW NOW - Show Channel\"
+    $time = strtotime(preg_replace("/~giggle +(\d{4}-\d{2}-\d{2} \d{1,2}:\d{2}).*/", "$1", $command));
+    $channel_name = preg_replace("/.*channel=([^ ]+).*/", "$1", $command);
+    $channel_id = $db->get_channel_by_name($channel_name, $guild_id);
+    if(is_null($channel_id)){
+      throw new BadRequestException("Cannot find channel ".$channel_name ." in ".$db->get_guild_name($guild_id)." server");
+    }
+    $desc = preg_replace("/.*desc=\"(.+)\".*/", "$1", $command);
+    $msg_id = substr(md5(rand().time()),0,8);
+    $db->create_message($msg_id, $guild_id, $channel_id, $time, $desc, $content, '', '');
+    $msg = $db->get_message_by_id($msg_id);
+    $message = new Message($msg[0], $msg[1], $msg[2], $msg[3], $msg[4], $msg[5], $msg[7], $msg[8], $msg[9]);
+    $message->set_all();
+    return $message;
+  }
+}
 ?>
--- web/save_message_response.php
+++ web/save_message_response.php
@@ -27,6 +27,7 @@ session_start();
 
 if(!isset($_SESSION['username'])){
   http_response_code(401);
+  print "It looks like your session has expired\nPlease refresh the page";
   exit;
 }
 
@@ -49,8 +50,6 @@ if($_POST['message_type']=='message' || $_POST['message_type']=='template'){
   print $myJSON;
 
 } elseif($_POST['message_type']=='batch'){
-  // http_response_code(501);
-  // print "TODO: Implement batch processing";
   $delim = substr(md5(time()),0,18);
   $input = preg_replace("/\n\n\+{20}\++\n\n/", $delim, $_POST['content']);
   $obj = explode($delim, $input);
@@ -58,20 +57,15 @@ if($_POST['message_type']=='message' || $_POST['message_type']=='template'){
   $db = new DBConnection();
   $messages = array();
   foreach($obj as $msg){
-    [ $command, $content ] = explode("\n", $msg, 2);
-    // ~giggle 2021-02-05 18:00 channel=truth-wanted desc=\"TW NOW - Show Channel\"
-    $time = strtotime(preg_replace("/~giggle +(\d{4}-\d{2}-\d{2} \d{1,2}:\d{2}).*/", "$1", $command));
-    $channel_name = preg_replace("/.*channel=([^ ]+).*/", "$1", $command);
-    $channel_id = $db->get_channel_by_name($channel_name, $_POST['server_id']);
-    if(is_null($channel_id)){
+    try {
+      $message = Message::create_message_from_command($msg, $_POST['server_id']);
+    }
+    catch(BadRequestException $e) {
       http_response_code(400);
-      print "Cannot find channel ".$channel_name;
+      print $e->getMessage();
       exit();
     }
-    $desc = preg_replace("/.*desc=\"(.+)\".*/", "$1", $command);
-    $msg_id = substr(md5(rand().time()),0,8);
-    $db->create_message($msg_id, $_POST['server_id'], $channel_id, $time, $desc, $content, '', '');
-    array_push($messages, $msg_id);
+    array_push($messages, $message->id);
   }
   print "Created ".count($messages)." messages\n".json_encode($messages);
 }
