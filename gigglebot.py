942ce137e1eb83c4c74d38a5259647c83e9f9db9
Tom Gigler <tgigler@esri.com>
2021-02-23 22:57:57 +0000

    Revert "Update mute_members table on server mute"
    This reverts commit 15ffc86e0e52227a824e3fe09bacff193076f4a0.

--- gigglebot.py
+++ gigglebot.py
@@ -985,10 +985,6 @@ async def on_voice_state_update(member, before, after):
         if not giguser.vips[(member.id, member.guild.id)].last_sent or time() - giguser.vips[(member.id, member.guild.id)].last_sent > grace_period:
             giguser.vips[(member.id, member.guild.id)].set_last_sent(time())
             await process_delay_message({'guild': member.guild, 'request_message_id': time(), 'delay': '0', 'from_template': giguser.vips[(member.id, member.guild.id)].template_id })
-    if before.mute and not after.mute:
-        gigdb.delete_mute_member(member.guild.id, member.id)
-    if not before.mute and after.mute:
-        gigdb.add_mute_member(member.guild.id, member.id, member.name)
 
 @client.event
 async def on_ready():
